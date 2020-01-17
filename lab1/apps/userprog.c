257fb9199e9a245bd901a4bb997a00e2e057e3c2
jltejada98 <tejada@purdue.edu>
2020-01-17 10:20:11 -0500

    Lab 1 (Corrected compilation error,2)

--- lab1/apps/userprog.c
+++ lab1/apps/userprog.c
@@ -3,6 +3,6 @@
 void main (int x)
 {
   Printf("Hello World!\n");
-  Printf("The PID of this user program is : %d", getpid());
+  Printf("The PID of this user program is : %d", Getpid());
   while(1); // Use CTRL-C to exit the simulator
 }
