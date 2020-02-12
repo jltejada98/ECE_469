8c49c2ab49e734acdd4cbf6dac9d2c2e4680d00c
jodom988 <jdomajors@gmail.com>
2020-02-12 16:48:45 -0500

    changed names

--- lab2/apps/q2/producer/producer.c
+++ lab2/apps/q2/producer/producer.c
@@ -7,14 +7,14 @@
 int main(int argc, char const *argv[])
 {
   //circular_buffer *cb
-  sem_t s_procs_completed; 
+  sem_t sem_procs_completed; 
 
   // if (argc != 3) { 
   //   Printf("Usage: "); Printf(argv[0]); Printf(" <handle_to_shared_memory_page> <handle_to_page_mapped_semaphore>\n"); 
   //   Exit();
   // } 
 
-  s_procs_completed = dstrtol(argv[1], NULL, 10);
+  sem_procs_completed = dstrtol(argv[1], NULL, 10);
 
   Printf("Producer_Created\n");
 
@@ -27,8 +27,8 @@ int main(int argc, char const *argv[])
   // }
 
   //Signal semaphore
-  if(sem_signal(s_procs_completed) != SYNC_SUCCESS){
-    Printf("Bad semaphore s_procs_completed (%d) in ", s_procs_completed); 
+  if(sem_signal(sem_procs_completed) != SYNC_SUCCESS){
+    Printf("Bad semaphore sem_procs_completed (%d) in ", sem_procs_completed); 
     Printf(argv[0]); 
     Printf(", exiting...\n");
     Exit();
