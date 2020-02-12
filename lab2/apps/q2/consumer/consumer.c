cdd85ea22904cabe84c4dfc4e3bacfdea5410c8c
jltejada98 <tejada@purdue.edu>
2020-02-12 15:25:50 -0500

    Lab2.2.1

---
+++ lab2/apps/q2/consumer/consumer.c
@@ -0,0 +1,24 @@
+#include "lab2-api.h"
+#include "usertraps.h"
+#include "misc.h"
+
+#include "spawn.h"
+
+int int main(int argc, char const *argv[])
+{
+  //circular_buffer *cb
+
+  for(int i = 0; i < 11; i++)
+  {
+    //Get lock
+    //Consume resource
+    //Release lock
+  }
+
+  //Signal semaphore
+
+
+
+
+  return 0;
+}
