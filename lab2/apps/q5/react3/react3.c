8892eee63889a4a5963dcb4741375683230b2d89
jodom988 <jdomajors@gmail.com>
2020-02-16 23:04:21 -0500

    Finshed react 3

--- lab2/apps/q5/react3/react3.c
+++ lab2/apps/q5/react3/react3.c
@@ -10,40 +10,57 @@
 
 int main(int argc, char const *argv[])
 {
-  buffer *cb;
-  sem_t sem_procs_completed; 
-  lock_t buffer_lock;
-  uint32 h_mem;                   // Used to hold handle to shared memory page
+  sem_t sem_procs_completed, sem_h, sem_0, sem_s02, sem_h_sulfate;
+  int numReact;
   int i;
 
-  if (argc != 4) { 
-    Printf("Usage: "); Printf(argv[0]); Printf(" <handle_to_shared_memory_page> <handle_to_page_mapped_semaphore>\n"); 
+  if (argc != 7) { 
+    Printf("Incorrect Arguments for %s", argv[0]);
     Exit();
   } 
 
   sem_procs_completed = dstrtol(argv[1], NULL, 10);
-  h_mem = dstrtol(argv[2], NULL, 10);
-  buffer_lock = dstrtol(argv[3], NULL, 10);
+  sem_h = dstrtol(argv[2], NULL, 10);
+  sem_o = dstrtol(argv[3], NULL, 10);
+  sem_s02 = dstrtol(argv[4], NULL, 10);
+  sem_h_sulfate = dstrtol(argv[5], NULL, 10);
+  numReact = dstrtol(argv[6], NULL, 10);
 
-  cb = (buffer*) shmat(h_mem);
-  if(cb == NULL)
+  for(i = 0; i < numReact; i++)
   {
-    Printf("Could not map virtual address to memory in ");
-    Printf(argv[0]);
-    Exit();
-  }
-
-  i = 0;
-  while(i < 11)
-  {
-    lock_acquire(buffer_lock);
-    if(cb->start != cb->end) //Buffer not empty
+    //Wait for resources
+    if(sem_wait(sem_h) != SYNC_SUCCESS)
     {
-      Printf("Consumer %d removed: %c\n", getpid(), cb->data[cb->start]);
-      cb->start = (cb->start + 1) % BUFFER_SIZE;
-      i++;
+      Printf("Bad semaphore sem_procs_completed (%d) in ", sem_procs_completed); 
+      Printf(argv[0]); 
+      Printf(", exiting...\n");
+      Exit();
     }
-    lock_release(buffer_lock);
+    if(sem_wait(sem_o) != SYNC_SUCCESS)
+    {
+      Printf("Bad semaphore sem_procs_completed (%d) in ", sem_procs_completed); 
+      Printf(argv[0]); 
+      Printf(", exiting...\n");
+      Exit();
+    }
+    if(sem_wait(sem_s02) != SYNC_SUCCESS)
+    {
+      Printf("Bad semaphore sem_procs_completed (%d) in ", sem_procs_completed); 
+      Printf(argv[0]); 
+      Printf(", exiting...\n");
+      Exit();
+    }
+
+    //Create resources
+    if(sem_signal(sem_h_sulfate) != SYNC_SUCCESS)
+    {
+      Printf("Bad semaphore sem_procs_completed (%d) in ", sem_procs_completed); 
+      Printf(argv[0]); 
+      Printf(", exiting...\n");
+      Exit();
+    }
+
+    Printf("(3) H2 + O2 + SO2 -> H2SO4 reacted, PID: %d", getpid());
   }
 
   //Signal semaphore
