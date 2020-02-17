bf78a1e880f0b591b440d68347bba3b2f226fd1e
jltejada98 <tejada@purdue.edu>
2020-02-16 21:18:13 -0500

    Lab 2 Question 5 Process Creation Completed

--- lab2/apps/q5/makeprocs/makeprocs.c
+++ lab2/apps/q5/makeprocs/makeprocs.c
@@ -7,7 +7,7 @@
 
 int main (int argc, char *argv[]){
     int numprocs = 5;
-    lock_t atm_lock; //Lock for atmospehere
+    lock_t atm_lock; //Lock for atmosphere
     sem_t sem_procs_sleeping; // Semaphore used to wait until all spawned processes have completed
     atm a; //Atmosphere data structure
     int num_water = 0;
@@ -16,6 +16,8 @@ int main (int argc, char *argv[]){
     char atm_lock_str[10];   //Used as command-line argument for lock
     char h_mem_str[10];             // Used as command-line argument to pass mem_handle to new processes
     char sem_procs_sleeping_str[10]; // Used as command-line argument to pass page_mapped handle to new processes
+    char num_water_str[10];
+    char num_sulfate_str[10];
     int i;
 
     if (argc != 2) {
@@ -68,11 +70,16 @@ int main (int argc, char *argv[]){
     ditoa(h_mem, h_mem_str);
     ditoa(sem_procs_sleeping, sem_procs_sleeping_str);
     ditoa(atm_lock, atm_lock_str);
+    ditoa(num_water, num_water_str);
+    ditoa(num_sulfate, num_sulfate_str);
 
+    //All of process creation
+    process_create(PRODUCER_FILENAME_1,sem_procs_sleeping_str, h_mem_str, atm_lock_str, num_water_str, NULL);
+    process_create(PRODUCER_FILENAME_2,sem_procs_sleeping_str, h_mem_str, atm_lock_str, num_sulfate_str , NULL);
+    process_create(REACT_FILENAME_1,sem_procs_sleeping_str, h_mem_str, atm_lock_str, NULL);
+    process_create(REACT_FILENAME_2,sem_procs_sleeping_str, h_mem_str, atm_lock_str, NULL);
+    process_create(REACT_FILENAME_3,sem_procs_sleeping_str, h_mem_str, atm_lock_str, NULL);
 
-    process_create(PRODUCER_FILENAME,sem_procs_sleeping_str, h_mem_str, atm_lock_str, NULL);
-    process_create(CONSUMER_FILENAME,sem_procs_sleeping_str, h_mem_str, atm_lock_str, NULL);
-    
 
     if (sem_wait(sem_procs_sleeping) != SYNC_SUCCESS) {
       Printf("Bad semaphore sem_procs_sleeping (%d) in ", sem_procs_sleeping); Printf(argv[0]); Printf("\n");
