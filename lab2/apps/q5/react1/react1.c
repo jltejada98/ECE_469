fd6ddd1f4d85bcd09ee7dbd174151ed0ed9a6d6e
jodom988 <jdomajors@gmail.com>
2020-02-16 22:55:38 -0500

    ASDF

--- lab2/apps/q5/react1/react1.c
+++ lab2/apps/q5/react1/react1.c
@@ -10,40 +10,39 @@
 
 int main(int argc, char const *argv[])
 {
-  buffer *cb;
-  sem_t sem_procs_completed; 
-  lock_t buffer_lock;
-  uint32 h_mem;                   // Used to hold handle to shared memory page
+  sem_t sem_procs_completed, sem_t sem_water, sem_h, sem_o;
+  int numReact;
   int i;
 
-  if (argc != 4) { 
-    Printf("Usage: "); Printf(argv[0]); Printf(" <handle_to_shared_memory_page> <handle_to_page_mapped_semaphore>\n"); 
+  if (argc != 5) { 
+    Printf("Incorrect Arguments for %s", argv[0]);
     Exit();
   } 
 
   sem_procs_completed = dstrtol(argv[1], NULL, 10);
-  h_mem = dstrtol(argv[2], NULL, 10);
-  buffer_lock = dstrtol(argv[3], NULL, 10);
+  sem_water = dstrtol(argv[2], NULL, 10);
+  sem_h = dstrtol(argv[3], NULL, 10);
+  sem_o = dstrtol(argv[4], NULL, 10);
+  numReact = dstrtol(argv[5], NULL, 10);
 
-  cb = (buffer*) shmat(h_mem);
-  if(cb == NULL)
+  for(i = 0; i < numReact; i++)
   {
-    Printf("Could not map virtual address to memory in ");
-    Printf(argv[0]);
-    Exit();
-  }
-
-  i = 0;
-  while(i < 11)
-  {
-    lock_acquire(buffer_lock);
-    if(cb->start != cb->end) //Buffer not empty
+    if(sem_wait(sem_water) != SYNC_SUCCESS)
     {
-      Printf("Consumer %d removed: %c\n", getpid(), cb->data[cb->start]);
-      cb->start = (cb->start + 1) % BUFFER_SIZE;
-      i++;
+			Printf("Bad semaphore sem_procs_completed (%d) in ", sem_procs_completed); 
+			Printf(argv[0]); 
+			Printf(", exiting...\n");
+			Exit();
     }
-    lock_release(buffer_lock);
+    if(sem_wait(sem_water) != SYNC_SUCCESS)
+    {
+			Printf("Bad semaphore sem_procs_completed (%d) in ", sem_procs_completed); 
+			Printf(argv[0]); 
+			Printf(", exiting...\n");
+			Exit();
+    }
+
+    Printf("(1) H2O -> 2 H2 + O2 reacted, PID: %d", getpid());
   }
 
   //Signal semaphore
