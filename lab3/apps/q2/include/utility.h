e84fa5286ea1a52796d26b4b240ef0e395fe8e8b
jodom988 <jdomajors@gmail.com>
2020-03-04 16:55:05 -0500

    Updating

--- lab3/apps/q2/include/utility.h
+++ lab3/apps/q2/include/utility.h
@@ -1,5 +1,5 @@
-#define PRODUCER_FILENAME_1 "producerS.dlx.obj"
-#define PRODUCER_FILENAME_2 "producerCO.dlx.obj"
+#define PRODUCER_FILENAME_S "producerS.dlx.obj"
+#define PRODUCER_FILENAME_CO "producerCO.dlx.obj"
 #define REACT_FILENAME_1 "react1.dlx.obj"
 #define REACT_FILENAME_2 "react2.dlx.obj"
 #define REACT_FILENAME_3 "react3.dlx.obj"
