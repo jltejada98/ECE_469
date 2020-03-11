b5d9eff6afebba8c5993470286f606d113fe91c1
jodom988 <jdomajors@gmail.com>
2020-03-10 22:41:59 -0400

    Fix naming in producerS.c

--- lab3/apps/q2/producerS/producerS.c
+++ lab3/apps/q2/producerS/producerS.c
@@ -6,7 +6,7 @@
   {
   sem_t sem_procs_completed; 
   int num_to_create;
-  mbox_t mbox_S;
+  mbox_t mbox_S2;
   int msg;
   int i;
 
@@ -22,18 +22,18 @@
 
   sem_procs_completed = dstrtol(argv[1], NULL, 10);
   num_to_create = dstrtol(argv[2], NULL, 10);
-  mbox_S = dstrtol(argv[3], NULL, 10);
+  mbox_S2 = dstrtol(argv[3], NULL, 10);
 
-  mbox_open(mbox_S);
+  mbox_open(mbox_S2);
 
   msg = 1;
   for(i = 0; i < num_to_create; i++)
   {
-    mbox_send(mbox_S, sizeof(int), &msg);
+    mbox_S2end(mbox_S2, sizeof(int), &msg);
     Printf("S2 injected into Radeon atmosphere, PID: %d \n", getpid());
   }
 
-  mbox_close(mbox_S);
+  mbox_close(mbox_S2);
 
   //Signal complete semaphore
   if(sem_signal(sem_procs_completed) != SYNC_SUCCESS){
