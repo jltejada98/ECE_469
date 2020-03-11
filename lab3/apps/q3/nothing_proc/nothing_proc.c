4fcd1ca03348c2eadebe14ab93b431fe11903953
jodom988 <jdomajors@gmail.com>
2020-03-11 16:09:42 -0400

    Added pid to proc exiting message

--- lab3/apps/q3/nothing_proc/nothing_proc.c
+++ lab3/apps/q3/nothing_proc/nothing_proc.c
@@ -21,7 +21,7 @@ int main(int argc, char const *argv[])
 
   k = 0;
 
-  Printf("entering loop\n");
+  Printf("entering loop (pid: %d) of size %d\n", getpid(), i);
   for(i = 0; i < num; i++)
   {
     for(j = 0; j < 100; j++)
