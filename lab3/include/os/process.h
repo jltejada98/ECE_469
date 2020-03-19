058024f5c2d0963ffcbdc36ce3962b2e63807552
jodom988 <jdomajors@gmail.com>
2020-03-19 13:17:38 -0400

    Fix error

--- lab3/include/os/process.h
+++ lab3/include/os/process.h
@@ -109,7 +109,7 @@ void ProcessUserSleep(int seconds);
 void ProcessYield();
 
 //Our functions
-void ProcessComputePriority (Link* l);
+void ProcessComputePriority (Link** l);
 void ProcessDecayEstCPUs (Queue* currQueue);
 Queue* GetPriorityQueue(PCB* pcb);
 
