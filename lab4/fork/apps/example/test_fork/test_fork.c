f7bef0d6aacde774112951b737494874529c33bb
jodom988 <jdomajors@gmail.com>
2020-04-14 12:16:59 -0400

    Q3 and Q4 working?

--- lab4/fork/apps/example/test_fork/test_fork.c
+++ lab4/fork/apps/example/test_fork/test_fork.c
@@ -20,13 +20,15 @@ void main (int argc, char *argv[])
   	Printf("This is the child (%d), child_proc=%d\n", getpid(), child_proc);
   	val += 2;
   	Printf("Val: %d, PID: %d\n", val, getpid());
+    Printf(">>>>>Success Child!<<<<<\n");
   }
   else{ //Parent Process
     Printf("This is the parent (%d), child_proc=%d\n", getpid(), child_proc);
     val += 5;
     Printf("Val: %d, PID: %d\n", val, getpid());
+    Printf(">>>>>>Success Parent!<<<<<\n");
+
   }
 
 
-  Printf("Success!\n");
 }
