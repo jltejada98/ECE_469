6b63b006512645d72c32e6d963f705f7150a98f9
jltejada98 <tejada@purdue.edu>
2020-04-14 00:47:31 -0400

    Lab 4 -Q4 test code

--- lab4/fork/apps/example/test_fork/test_fork.c
+++ lab4/fork/apps/example/test_fork/test_fork.c
@@ -4,5 +4,26 @@
 
 void main (int argc, char *argv[])
 {
+  int child_proc = 0;
+  int val = 0;
+
   Printf("Starting test_fork\n");
+
+  child_proc = fork();
+
+  if (child_proc == 0) //Child Process
+  {
+  	Printf("child_proc with pid:", getpid());
+  	Printf("Generating ROP exception.. Original Value: %d, New Value", val, val+2)
+  	val += 2;
+  	Printf("Finished ROP exception, Val: %d, PID:", val, getpid());
+  }
+  else{ //Parent Process
+  	Printf("parent_proc with pid:", getpid());
+  	Printf("Generating ROP exception.. Original Value: %d, New Value", val, val+1)
+  	++val;
+  	Printf("Finished ROP exception, Val: %d, PID:", val, getpid());
+  }
+
+
 }
