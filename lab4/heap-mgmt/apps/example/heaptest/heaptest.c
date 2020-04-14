81ce36f8d2e7e18a9c07b36e2fc1ab54f3df2251
jltejada98 <tejada@purdue.edu>
2020-04-14 18:57:50 -0400

    Lab4 - added heaptest files

---
+++ lab4/heap-mgmt/apps/example/heaptest/heaptest.c
@@ -0,0 +1,33 @@
+#include "usertraps.h"
+#include "misc.h"
+
+void main (int argc, char *argv[])
+{
+  int num_alloc = 40;
+  int *array;
+
+  //Attempt to allocate memory.
+  Printf("Heaptest: Attempting to allocated %d bytes.\n", num_alloc * sizeof(int));
+  array = malloc(sizeof(int) * num_alloc);
+  if (array == NULL)
+  {
+    Printf("Heaptest: could not allocate heap. Exiting");
+    return; //Exit ??
+  }
+  Printf("Heaptest: Successfully allocate %d bytes at address (decimal) %d\n", num_alloc * sizeof(int), array);
+
+  //Write to memory (Just checking validity of address)
+  Printf("Writing to allocated memory...\n");
+  array[0] = 1;
+  Printf("Successfully wrote to to allocated memory...\n");
+
+  //Attempt to free memory.
+  Printf("Heaptest: Attempting to free %d bytes.\n", num_alloc * sizeof(int));
+  if(free(array) == MEM_FAIL)
+  {
+    Printf("Heaptest: could not free heap. Exiting");
+    return; //Exit ??
+  }
+  Printf("Heaptest: Successfully freed %d bytes.\n", num_alloc * sizeof(int));
+
+}
