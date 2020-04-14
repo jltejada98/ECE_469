4bc9083b5d86fb503cb2c65009222af52a66b047
jodom988 <jdomajors@gmail.com>
2020-04-14 00:50:33 -0400

    Fix errors

--- lab4/fork/include/os/process.h
+++ lab4/fork/include/os/process.h
@@ -103,7 +103,7 @@ void ProcessKill();
 //-------------------------------------------------------
 // Put any functions prototypes that you define here.
 //-------------------------------------------------------
-
-
+int ProcessRealFork(PCB* parent);
+printPtes(PCB* proc1, PCB* proc2);
 
 #endif	/* __process_h__ */
